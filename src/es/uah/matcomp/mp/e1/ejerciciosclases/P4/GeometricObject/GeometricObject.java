cfba5033d59d166e84bf15488b85eb7f0bb75621
danielarojast11 <daniela.rojast@edu.uah.es>
2024-03-04 20:47:50 +0100

    practica 3

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/P4/GeometricObject/GeometricObject.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/P4/GeometricObject/GeometricObject.java
@@ -1,6 +1,7 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases.P4.GeometricObject;
 
-public interface GeometricObject {
+public interface GeometricObject {      //Interfaz != clase abstracta
     double getArea();
-    double getPerimeter();
+    double getPerimeter();      //Métodos no imlementados/inicalizados,
+                                // los implementan las subclases
 }
