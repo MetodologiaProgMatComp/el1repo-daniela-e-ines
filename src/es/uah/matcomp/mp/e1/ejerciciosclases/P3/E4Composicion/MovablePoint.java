371d1f1e9fa86df9f2e334984d432bb8f2984e3f
inesperezpascual <ines.perezpascual@edu.uah.es>
2024-03-04 01:02:33 +0100

    P3 algunos composición

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/P3/E4Composicion/MovablePoint.java
@@ -0,0 +1,63 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P3.E4Composicion;
+
+import es.uah.matcomp.mp.e1.ejerciciosclases.P3.E4Composicion.Point;
+
+public class MovablePoint extends Point {
+    private Point point;
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        this.point = new Point(x,y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.point = new Point();
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {this.point = new Point();}
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] resultado = new float[2];
+        resultado[0] = xSpeed;
+        resultado[1] = ySpeed;
+        return resultado;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public String toString() {
+        return super.toString() + "speed=(" + xSpeed + ',' + ySpeed + ')';
+    }
+
+    public MovablePoint move() {
+        float newX = point.getX() + xSpeed;
+        float newY = point.getY() + ySpeed;
+        setXY(newX,newY);
+        return this;
+    }
+}
+
---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/P3/E5Composicion/Circle.java
@@ -0,0 +1,50 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P3.E5Composicion;
+
+import es.uah.matcomp.mp.e1.ejerciciosclases.P3.E5.Shape;
+
+public class Circle extends Shape {
+    private Shape shape;
+    protected double radius = 1.0;
+
+    public Circle() {this.shape = new Shape();}
+    public Circle(double radius) {
+        this.shape = new Shape();
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.shape = new Shape(color,filled);
+        this.radius = radius;
+    }
+
+    @Override
+    public String getColor() {
+        return shape.getColor();
+    }
+
+    @Override
+    public boolean isFilled() {
+        return shape.isFilled();
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" + shape.toString() + ",radius=" + radius + ']';
+    }
+}
